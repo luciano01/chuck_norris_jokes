3de2e8de3e9c97f57198fb1847de6a415b1d8eed
Luciano <luciano@idolink.bio>
2023-02-27 19:24:49 -0300

    Add initial structure

---
+++ lib/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'features/presentation/presentation.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ChuckNorris API',
+      home: HomePage(),
+    );
+  }
+}
